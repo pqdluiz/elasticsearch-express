32352f4a2443ad5874f90106b115f8246db5cb8e
luizaoformigapqd <luizlima@garantti.com.br>
2022-04-05 14:10:34 -0300

    test: 💍 gar-04/create tests configurations

---
+++ src/__tests__/db.controler.spec.ts
@@ -0,0 +1,5 @@
+import supertest from "supertest";
+
+describe("DBController", () => {
+  it("Create", () => {});
+});
---
+++ src/__tests__/photo.controller.spec.ts
@@ -0,0 +1,13 @@
+import supertest from "supertest";
+
+describe("PhotoController", () => {
+  it("Create", () => {});
+
+  it("Find All", () => {});
+
+  it("Find all Query", () => {});
+
+  it("Find by ID", () => {});
+
+  it("Create photo", () => {});
+});
